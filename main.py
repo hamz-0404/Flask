8789385a1ee2f65dcbcde5d276180c72f1b52bb0
hamz-0404 <hamzapvt164@gmail.com>
2024-10-14 19:54:16 +0530

    Flask Inhertance

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('Index.html')
+
+@app.route("/About")
+def aboutus():
+    return render_template('about.html')
+
+if __name__== ("__main__") :
+    app.run()
